2fdd7d0dbeed983eaa9c416f85665d63f0306234
LizSantana <liz.santanabrito@gmail.com>
2021-11-11 16:39:47 -0300

    antes de subir para heroku

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import { router as locacaoRouter} from '../src/routes/locacao.routes';
-import express from 'express';
+import express, { urlencoded, json } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import errorHandler from 'errorhandler';
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
-app.use('/', locacaoRouter);
+app.use(cors());
+app.use(json());
+app.use(urlencoded({extended: false}));
+if(process.env.NODE_ENV !== 'production'){
+    app.use(morgan('dev'));
+    app.use(errorHandler());
+}else{
+    app.use(morgan('tiny'));
+}
+
+app.use(`/api`, locacaoRouter);
 
 export default app;
--- src/controllers/locacao.controller.ts
+++ src/controllers/locacao.controller.ts
@@ -5,42 +5,42 @@ import * as locacaoRepositorio from '../persistencia/locacaoRepositorio';
 
 export async function getLocacao(req: Request, res: Response) {
     const locacao = await LocacaoModel.find().lean();
-    res.json(locacao);
+    return res.json(locacao);
 }
 
 export async function getFiltragemPorUF(req: Request, res: Response) {
     const uf = req.params.uf;
     const resultado = await locacaoRepositorio.buscarPorUF(uf);
-    res.json(resultado);
+    return res.json(resultado);
 }
 
 export async function getFiltragemPorLocalidade(req: Request, res: Response) {
     const localidade = req.params.localidade;
     const resultado = await locacaoRepositorio.buscarPorLocalidade(localidade);
-    res.json(resultado);
+    return res.json(resultado);
 }
 
 export async function getFiltragemPorCapacidade(req: Request, res: Response) {
     const capacidade = parseInt(req.params.capacidade);
     const resultado = await locacaoRepositorio.buscarPorCapacidade(capacidade);
-    res.json(resultado);
+    return res.json(resultado);
 }
 
 export async function getFiltragemPorPreco(req:Request, res: Response) {
     const preco = parseInt(req.params.preco);
     const resultado = await locacaoRepositorio.buscarPorPreco(preco);
-    res.json(resultado);
+    return res.json(resultado);
 }
 
 export async function postLocacao(req: Request, res: Response) {
     const erros = validationResult(req);
     if (!erros.isEmpty()) {
-        res.status(400).json({erros: erros.array()});
+       return res.status(400).json({erros: erros.array()});
     } else {
     const locacao = req.body;
     if (locacao) {
         await locacaoRepositorio.inserirLocacao(locacao);
-        res.send(locacao);
+       return res.send(locacao);
     }
     }
 }
@@ -48,10 +48,10 @@ export async function postLocacao(req: Request, res: Response) {
 export function postAloValidado(req: Request, res: Response) {
     const erros = validationResult(req);
     if (!erros.isEmpty()) {
-        res.status(400).json({erros: erros.array()});
+        return res.status(400).json({erros: erros.array()});
     } else {
         const {nome} = req.body;
-        res.send(`Alô, ${nome}!`);
+        return res.send(`Alô, ${nome}!`);
     }
 }
 
