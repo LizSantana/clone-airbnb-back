6b57ce4694a2822d2a899c05951f08952f3a7d34
LizSantana <liz.santanabrito@gmail.com>
2021-11-11 21:45:21 -0300

    adicionando metodo post

--- src/routes/locacao.routes.ts
+++ src/routes/locacao.routes.ts
@@ -11,6 +11,6 @@ router.get(`${path}/uf/:uf`, LocacaoController.getFiltragemPorUF);
 router.get(`${path}/localidade/:localidade`, LocacaoController.getFiltragemPorLocalidade);
 router.get(`${path}/capacidade/:capacidade`, LocacaoController.getFiltragemPorCapacidade);
 router.get(`${path}/preco/:preco`, LocacaoController.getFiltragemPorPreco);
-/*router.post(`${path}/validado`,
-            body('nome').notEmpty().trim().isLength({min:1, max:25}).withMessage('Nome deve ter entre 1 e 25 caracteres'),
-            AloController.postAloValidado);*/
+router.post(`${path}/criarlocacao`,
+            /*body('nome').notEmpty().trim().isLength({min:1, max:25}).withMessage('Nome deve ter entre 1 e 25 caracteres'),*/
+            LocacaoController.postLocacao);
