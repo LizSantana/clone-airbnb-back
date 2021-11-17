55d533c26b4799c4cee89f0a3941f8574d45be16
LizSantana <liz.santanabrito@gmail.com>
2021-11-17 11:03:34 -0300

    retirando comentarios de routes

--- src/routes/locacao.routes.ts
+++ src/routes/locacao.routes.ts
@@ -6,21 +6,11 @@ import uploads from '../upload';
 export const router = Router();
 export const path = '/locacao';
 router.get(path, LocacaoController.getLocacao);
-//router.get(`${path}/erro`, AloController.getAloErro);
 router.get(`${path}/uf/:uf`, LocacaoController.getFiltragemPorUF);
 router.get(`${path}/localidade/:localidade`, LocacaoController.getFiltragemPorLocalidade);
 router.get(`${path}/capacidade/:capacidade`, LocacaoController.getFiltragemPorCapacidade);
 router.get(`${path}/preco/:preco`, LocacaoController.getFiltragemPorPreco);
 router.post(`${path}/criarlocacao`,
-            /*body('nome').notEmpty().trim().isLength({min:1, max:25}).withMessage('Nome deve ter entre 1 e 25 caracteres'),*/
             LocacaoController.postLocacao);
 router.post(`${path}/:id`, LocacaoController.putLocacao);
 router.post(`${path}/:id/deletarlocacao`, LocacaoController.deleteLocacao);
-/*router.post('/postPhoto', uploads.single('image'), async function (req: Request, res: Response, next: NextFunction) {
-    const obj = {
-        img: {
-            data
-        }
-    }
-    await console.log('post');
-    });*/
